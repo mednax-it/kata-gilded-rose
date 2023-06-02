acfe0804c03ee00b45b9896c63352167ef1e4a15
Gabriel Cote-Carrier <gabriel.cotecarrier@pediatrix.com>
2023-06-02 11:16:19 -0400

    Unwrap the aged brie and backstages passes bottom logic

--- gilded_rose.py
+++ gilded_rose.py
@@ -36,20 +36,18 @@ class GildedRose:
             GildedRose.update_sell_in(item)
 
             if item.sell_in < 0:
-                if "Aged Brie" != item.name:
-                    if "Backstage passes to a TAFKAL80ETC concert" != item.name:
-                        if item.quality > 0:
-                            if "Sulfuras, Hand of Ragnaros" != item.name:
-                                item.quality = item.quality - 1
-                    else:
-                        # TODO: Fix this.
-                        item.quality = item.quality - item.quality
-                else:
+                if "Aged Brie" == item.name:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-                    if "Aged Brie" == item.name and item.sell_in <= 0:
+                    if item.sell_in <= 0:
                         item.quality = 0
-                        # of for.
+                elif "Backstage passes to a TAFKAL80ETC concert" == item.name:
+                    # TODO: Fix this.
+                    item.quality = 0
+                else:
+                    if item.quality > 0:
+                        if "Sulfuras, Hand of Ragnaros" != item.name:
+                            item.quality = item.quality - 1
             if "Sulfuras, Hand of Ragnaros" != item.name:
                 if item.quality > 50:
                     item.quality = 50
