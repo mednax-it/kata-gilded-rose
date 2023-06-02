921f975d1364a1cef3ee8a4b3d729f954b2d04f6
Gabriel Cote-Carrier <gabriel.cotecarrier@pediatrix.com>
2023-06-02 11:22:54 -0400

    Move all quality manipulation logic into a single block

--- gilded_rose.py
+++ gilded_rose.py
@@ -17,6 +17,11 @@ class GildedRose:
                         item.quality = item.quality + 2
                     else:
                         item.quality = item.quality + 1
+                if item.sell_in <= 0:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                    if item.sell_in <= 1:
+                        item.quality = 0
             elif "Backstage passes to a TAFKAL80ETC concert" == item.name:
                 if item.quality < 50:
                     if item.sell_in <= 5:
@@ -25,30 +30,24 @@ class GildedRose:
                         item.quality = item.quality + 2
                     else:
                         item.quality = item.quality + 1
-
+                if item.sell_in <= 0:
+                    # TODO: Fix this.
+                    item.quality = 0
             elif "Sulfuras, Hand of Ragnaros" == item.name:
                 pass
             else:
                 # TODO: Improve this code.  Word.
                 if item.quality > 0:
                     item.quality = item.quality - 1
-
-            GildedRose.update_sell_in(item)
-
-            if item.sell_in < 0:
-                if "Aged Brie" == item.name:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                    if item.sell_in <= 0:
-                        item.quality = 0
-                elif "Backstage passes to a TAFKAL80ETC concert" == item.name:
-                    # TODO: Fix this.
-                    item.quality = 0
-                else:
+                if item.sell_in <= 0:
                     if item.quality > 0:
                         if "Sulfuras, Hand of Ragnaros" != item.name:
                             item.quality = item.quality - 1
+
             if "Sulfuras, Hand of Ragnaros" != item.name:
                 if item.quality > 50:
                     item.quality = 50
+
+            GildedRose.update_sell_in(item)
+
         return items
