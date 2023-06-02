8f0c2f68a2cdfb700e2d02a6ec10eb29f09074a1
Gabriel Cote-Carrier <gabriel.cotecarrier@pediatrix.com>
2023-06-02 10:50:52 -0400

    Update the sellin if statements to better reflect the documentation numbers

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,6 +8,7 @@ class GildedRose:
     @staticmethod
     def update_item(items):
         for item in items:
+
             if "Aged Brie" != item.name and "Backstage passes to a TAFKAL80ETC concert" != item.name:
                 # TODO: Improve this code.  Word.
                 if item.quality > 0:
@@ -17,22 +18,24 @@ class GildedRose:
                 if item.quality < 50:
                     item.quality = item.quality + 1
                     if "Aged Brie" == item.name:
-                        if item.sell_in < 6:
+                        if item.sell_in <= 5:
                             item.quality = item.quality + 1
                     # Increases the Quality of the stinky cheese if it's 11 days to due date.
                     if "Aged Brie" == item.name:
-                        if item.sell_in < 11:
+                        if item.sell_in <= 10:
                             item.quality = item.quality + 1
                     if "Backstage passes to a TAFKAL80ETC concert" == item.name:
-                        if item.sell_in < 11:
+                        if item.sell_in <= 10:
                             # See revision number 2394 on SVN.
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         # Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                        if item.sell_in < 6:
+                        if item.sell_in <= 5:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
+
             GildedRose.update_sell_in(item)
+
             if item.sell_in < 0:
                 if "Aged Brie" != item.name:
                     if "Backstage passes to a TAFKAL80ETC concert" != item.name:
