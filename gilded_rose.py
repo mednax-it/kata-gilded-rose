09666f6a6dd0b76ac0ce3934bb335199ed6eb0c2
Gabriel Cote-Carrier <gabriel.cotecarrier@pediatrix.com>
2023-06-02 11:01:57 -0400

    Extract out the product-specific quality increase logic

--- gilded_rose.py
+++ gilded_rose.py
@@ -9,30 +9,30 @@ class GildedRose:
     def update_item(items):
         for item in items:
 
-            if "Aged Brie" != item.name and "Backstage passes to a TAFKAL80ETC concert" != item.name:
-                # TODO: Improve this code.  Word.
-                if item.quality > 0:
-                    if "Sulfuras, Hand of Ragnaros" != item.name:
-                        item.quality = item.quality - 1
-            else:
+            if "Aged Brie" == item.name:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if "Aged Brie" == item.name:
-                        if item.sell_in <= 5:
+                    if item.sell_in <= 5:
+                        item.quality = item.quality + 1
+                    if item.sell_in <= 10:
+                        item.quality = item.quality + 1
+            elif "Backstage passes to a TAFKAL80ETC concert" == item.name:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.sell_in <= 10:
+                        # See revision number 2394 on SVN.
+                        if item.quality < 50:
                             item.quality = item.quality + 1
-                    # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                    if "Aged Brie" == item.name:
-                        if item.sell_in <= 10:
+                    # Increases the Quality of Backstage Passes if the Quality is 6 or less.
+                    if item.sell_in <= 5:
+                        if item.quality < 50:
                             item.quality = item.quality + 1
-                    if "Backstage passes to a TAFKAL80ETC concert" == item.name:
-                        if item.sell_in <= 10:
-                            # See revision number 2394 on SVN.
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        # Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                        if item.sell_in <= 5:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+            elif "Sulfuras, Hand of Ragnaros" == item.name:
+                pass
+            else:
+                # TODO: Improve this code.  Word.
+                if item.quality > 0:
+                    item.quality = item.quality - 1
 
             GildedRose.update_sell_in(item)
 
