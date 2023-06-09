315732e14d4e69b82d2a49bf1f2565dea544a1f2
Gabriel Cote-Carrier <gabriel.cotecarrier@pediatrix.com>
2023-06-09 10:45:09 -0400

    Extract specific update quality methods for aged brie and backstage passes

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,46 +8,55 @@ class GildedRose:
     @staticmethod
     def update_item(items):
         for item in items:
-
-            if "Aged Brie" == item.name:
-                if item.quality < 50:
-                    if item.sell_in <= 5:
-                        item.quality = item.quality + 3
-                    elif item.sell_in <= 10:
-                        item.quality = item.quality + 2
-                    else:
-                        item.quality = item.quality + 1
-                if item.sell_in <= 0:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                    if item.sell_in <= 1:
-                        item.quality = 0
-            elif "Backstage passes to a TAFKAL80ETC concert" == item.name:
-                if item.quality < 50:
-                    if item.sell_in <= 5:
-                        item.quality = item.quality + 3
-                    elif item.sell_in <= 10:
-                        item.quality = item.quality + 2
-                    else:
-                        item.quality = item.quality + 1
-                if item.sell_in <= 0:
-                    # TODO: Fix this.
-                    item.quality = 0
-            elif "Sulfuras, Hand of Ragnaros" == item.name:
-                pass
-            else:
-                # TODO: Improve this code.  Word.
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-                if item.sell_in <= 0:
-                    if item.quality > 0:
-                        if "Sulfuras, Hand of Ragnaros" != item.name:
-                            item.quality = item.quality - 1
-
-            if "Sulfuras, Hand of Ragnaros" != item.name:
-                if item.quality > 50:
-                    item.quality = 50
-
+            GildedRose.update_quality(item)
             GildedRose.update_sell_in(item)
 
         return items
+
+    @staticmethod
+    def update_quality(item):
+        if "Aged Brie" == item.name:
+            GildedRose.update_quality_aged_brie(item)
+        elif "Backstage passes to a TAFKAL80ETC concert" == item.name:
+            GildedRose.update_quality_backstage_passes(item)
+        elif "Sulfuras, Hand of Ragnaros" == item.name:
+            pass
+        else:
+            # TODO: Improve this code.  Word.
+            if item.quality > 0:
+                item.quality = item.quality - 1
+            if item.sell_in <= 0:
+                if item.quality > 0:
+                    if "Sulfuras, Hand of Ragnaros" != item.name:
+                        item.quality = item.quality - 1
+
+        if "Sulfuras, Hand of Ragnaros" != item.name:
+            if item.quality > 50:
+                item.quality = 50
+
+    @staticmethod
+    def update_quality_backstage_passes(item):
+        if item.quality < 50:
+            if item.sell_in <= 5:
+                item.quality = item.quality + 3
+            elif item.sell_in <= 10:
+                item.quality = item.quality + 2
+            else:
+                item.quality = item.quality + 1
+        if item.sell_in <= 0:
+            item.quality = 0
+
+    @staticmethod
+    def update_quality_aged_brie(item):
+        if item.quality < 50:
+            if item.sell_in <= 5:
+                item.quality = item.quality + 3
+            elif item.sell_in <= 10:
+                item.quality = item.quality + 2
+            else:
+                item.quality = item.quality + 1
+        if item.sell_in <= 0:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+            if item.sell_in <= 1:
+                item.quality = 0
